3180288278ee43ef6db5bad3f387a7d619f6d14b
Jared Mathes <j-mathes@users.noreply.github.com>
2020-09-09 22:32:31 -0600

    Update 20 - Self Assessment Exam 2 - Calculate Power and Remainder Using Pointers.cpp

--- Learn C++ from Scratch/20 - Self Assessment Exam 2 - Calculate Power and Remainder Using Pointers.cpp
+++ Learn C++ from Scratch/20 - Self Assessment Exam 2 - Calculate Power and Remainder Using Pointers.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+using namespace std;
 void passReference(int * a, int * b) 
 {
-    *a = pow(*a, *b);
+    cout << "inputs: " << *a << " " << *b << endl;
+    int p=1;
+    for(int i=0; i<*b; i++)
+    {
+        p=(p * *a);
+    }
+    *a = p;
+
     *b = *a % *b;
 } 
