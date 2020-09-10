48b5a1cebd063b7a297684402de71b4014799f21
Jared Mathes <j-mathes@users.noreply.github.com>
2020-09-09 21:20:26 -0600

    Create 05 - Computing Binomial Expression.cpp

---
+++ Learn C++ from Scratch/05 - Computing Binomial Expression.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+double test(double a, double b) 
+{
+  double computeSquares;
+  double computeProduct;
+  double answer;
+
+  computeSquares = pow(a, 2) + pow(b, 2);
+  computeProduct = 2*a*b;
+  answer = computeSquares + computeProduct;
+  cout << "Answer is: " << answer << endl;
+  return answer;
+}
