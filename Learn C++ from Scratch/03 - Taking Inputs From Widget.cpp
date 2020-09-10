d96a0341b8dee404f757aa21ecfa44dca26f9b4c
Jared Mathes <j-mathes@users.noreply.github.com>
2020-09-09 21:18:42 -0600

    Create 03 - Taking Inputs From Widget.cpp

---
+++ Learn C++ from Scratch/03 - Taking Inputs From Widget.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    string name;
+    string address;
+    string phoneNumber;
+    
+    cout << "Enter name: "<<endl;
+    cin >> name;
+    
+    cout << "Enter address: "<<endl;
+    cin >> address;
+    
+    cout << "Enter phone number: "<<endl;
+    cin>> phoneNumber;
+    
+    cout << "Name is: ";
+    cout << name<<endl;
+  
+    cout << "Address is: ";
+    cout << address << endl;
+  
+    cout << "Phone number is: ";
+    cout << phoneNumber << endl;
+    return 0;
+}
