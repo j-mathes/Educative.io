7fb42482f7277f68c54a32feebbb8515f4d808df
Jared Mathes <j-mathes@users.noreply.github.com>
2020-09-24 15:26:28 -0600

    Update 06 - Constant Expression Exercise 1.cpp

--- C++ Fundamentals for Professionals/06 - Constant Expression Exercise 1.cpp
+++ C++ Fundamentals for Professionals/06 - Constant Expression Exercise 1.cpp
@@ -1,13 +1,14 @@
 // Use the structure, MyDouble, in our program. How can we check that instances of MyDouble will be created at compile-time?
-// Is this right?
 #include <iostream>
 
 struct MyDouble{
   double myVal;
   constexpr MyDouble(double v): myVal(v){} 
-  constexpr double getVal(){ return myVal; }
+  // This is wrong: constexpr double getVal(){ return myVal; }
+  constexpr double getVal() const { return myVal; }
 };
 
 int main() {
-  constexpr MyDouble d(5.1);
+  constexpr MyDouble d{5.1};
+  static_assert(d.getVal() == 5.1, "bad static assert");
 }
