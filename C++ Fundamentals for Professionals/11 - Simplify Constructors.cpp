2bfbc5e5e07cffbdaceeea65d8e0cb420db69dfd
Jared Mathes <j-mathes@users.noreply.github.com>
2020-09-26 16:04:34 -0600

    Create 11 - Simplify Constructors.cpp

---
+++ C++ Fundamentals for Professionals/11 - Simplify Constructors.cpp
@@ -0,0 +1,75 @@
+//Original
+#include <iostream>
+
+class Widget{
+  public:
+    Widget(): width(640), height(480), frame(false), visible(true) {}
+
+    Widget(int w): width(w), height(getHeight(w)), frame(false), visible(true){}
+
+    Widget(int w, int h): width(w), height(h), frame(false), visible(true){}
+
+    void show(){ std::cout << std::boolalpha << width << "x" << height
+                           << ", frame: " << frame << ", visible: " << visible
+                           << std::endl;
+     }
+  private:
+    int getHeight(int w){ return w*3/4; }
+    int width;
+    int height;
+    bool frame;
+    bool visible;
+};
+
+int main(){
+
+  std::cout << std::endl;
+
+  Widget wVGA;
+  Widget wSVGA(800);
+  Widget wHD(1280, 720);
+
+  wVGA.show();
+  wSVGA.show();
+  wHD.show();
+
+  std::cout << std::endl;
+
+}
+
+// Solution
+#include <iostream>
+
+class Widget{
+  public:
+    Widget(){}
+    Widget(int w): width(w), height(getHeight(w)){}
+    Widget(int w, int h): width(w), height(h){}
+
+    void show(){ std::cout << std::boolalpha << width << "x" << height
+                           << ", frame: " << frame << ", visible: " << visible
+                           << std::endl;
+     }
+  private:
+    int getHeight(int w){ return w*3/4; }
+    int width = 640;
+    int height = 480;
+    bool frame = false;
+    bool visible = true;
+};
+
+int main(){
+
+  std::cout << std::endl;
+
+  Widget wVGA;
+  Widget wSVGA(800);
+  Widget wHD(1280, 720);
+
+  wVGA.show();
+  wSVGA.show();
+  wHD.show();
+
+  std::cout << std::endl;
+
+}
