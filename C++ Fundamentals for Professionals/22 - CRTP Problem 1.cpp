2031e82a7815e89f71b363b078defa800537dc11
Jared Mathes <j-mathes@users.noreply.github.com>
2020-10-11 15:55:58 -0600

    Create 22 - CRTP Problem 1.cpp

---
+++ C++ Fundamentals for Professionals/22 - CRTP Problem 1.cpp
@@ -0,0 +1,3 @@
+//Extend the given piece of code with a Person class. A Person should have a first and last name.
+//We need to create two objects of the Person class and compare these objects using relational operators.
+
