307899f9bc71892ead4a03b907ef4e48b2eb6c99
Jared Mathes <j-mathes@users.noreply.github.com>
2020-10-24 22:00:01 -0600

    Create 38 - Unordered Map Hash.cpp

---
+++ C++ Fundamentals for Professionals/38 - Unordered Map Hash.cpp
@@ -0,0 +1,34 @@
+// unorderedMapHash.cpp 
+#include <iostream>
+#include <unordered_map>
+
+struct MyInt{
+  MyInt(int v):val(v){}
+  bool operator== (const MyInt& other) const {
+    return val == other.val;
+  }
+  int val;
+};
+
+struct MyHash{
+  std::size_t operator()(MyInt m) const {
+    std::hash<int> hashVal;
+    return hashVal(m.val);
+  }
+};
+
+std::ostream& operator << (std::ostream& st, const MyInt& myIn){
+  st << myIn.val ;
+  return st;
+}
+
+int main(){
+typedef std::unordered_map<MyInt, int, MyHash> MyIntMap;
+MyIntMap myMap{{MyInt(-2), -2}, {MyInt(-1), -1}, {MyInt(0), 0}, {MyInt(1), 1}};
+
+for(auto m : myMap) std::cout << "{" << m.first << "," << m.second << "} ";
+    // {MyInt(1),1} {MyInt(0),0} {MyInt(-1),-1} {MyInt(-2),-2}
+
+std::cout << myMap[MyInt(-2)] << std::endl;     // -2
+return 0;
+}
